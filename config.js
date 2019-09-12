ddc0a1f5cadb72a4da1b25edefc1d4572e0ec37a
Sean Connolly <seanconnolly@ufl.edu>
2019-09-12 17:53:05 -0400

    Second

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: {
+      uri: 'mongodb+srv://bootcampUser:<bootcamp>@bootcamp2cluster-rbtvg.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+    }
+  };
