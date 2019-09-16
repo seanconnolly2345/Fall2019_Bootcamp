41a7129529297eb83d7ae9a355e0e6cc72645e0e
seanconnolly2345 <32102413+seanconnolly2345@users.noreply.github.com>
2019-09-16 15:13:36 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: {
-      uri: 'mongodb+srv://user1:bootcamp@bootcamp2cluster-rbtvg.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-  };
